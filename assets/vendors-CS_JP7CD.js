d4a17e86c78c1521c37fe12ae1bc84b3bc4460b8
s87345504 <39732550+s87345504@users.noreply.github.com>
2024-09-20 16:22:19 +0800

    update typeselected

--- assets/vendors-CS_JP7CD.js
+++ assets/vendors-CS_JP7CD.js
@@ -1,4 +1,4 @@
-import{g as getDefaultExportFromCjs,c as commonjsGlobal}from"./index-Ba4NYGVm.js";/**
+import{g as getDefaultExportFromCjs,c as commonjsGlobal}from"./index-BvqJ_jJD.js";/**
 * @vue/shared v3.4.21
 * (c) 2018-present Yuxi (Evan) You and Vue contributors
 * @license MIT
