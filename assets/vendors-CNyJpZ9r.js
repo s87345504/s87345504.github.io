17fdc2748aa02119b592a3716dfa5c5a4c3510a2
s87345504 <87345504@qq.com>
2025-02-02 16:42:22 +0800

    update version

--- assets/vendors-CNyJpZ9r.js
+++ assets/vendors-CNyJpZ9r.js
@@ -1,4 +1,4 @@
-import{g as getDefaultExportFromCjs,c as commonjsGlobal}from"./index-DSO95qdj.js";/**
+import{g as getDefaultExportFromCjs,c as commonjsGlobal}from"./index-DsQWnV3f.js";/**
 * @vue/shared v3.4.21
 * (c) 2018-present Yuxi (Evan) You and Vue contributors
 * @license MIT
